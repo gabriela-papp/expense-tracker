e5501314cfc378acfbe1b2eedff4f7265b547cc3
Gabriela Papp <gpgabika@gmail.com>
2022-12-28 19:57:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2 className="title">Expense Tracker</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;

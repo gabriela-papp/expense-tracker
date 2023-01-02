78871f36c5fa00b948c0884b2716bda287266997
Gabriela Papp <gpgabika@gmail.com>
2023-01-02 20:05:23 +0000

    new expense form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+
 import ExpenseItem from "./components/ExpenseItem";
+import NewExpense from "./components/NewExpense";
+ 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      
+    },
+  ];
   return (
     <div>
       <h2 className="title">Expense Tracker</h2>
-      <ExpenseItem/>
-    </div>
+      <NewExpense/>
+      {expenses.map((expense)=>{
+        return <ExpenseItem key={expense.id} expense={expense}> 
+        </ExpenseItem>
+      })}
+      </div>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import './ExpenseItem.style.css'
 
-const ExpenseItem=()=>{
+const ExpenseItem=(expense)=>{
+    const [item, setItem]= useState()
+const clickHandler=()=>{
+
+}
+
+    const {title,date,amount}=expense.expense
+    {console.log(expense.expense)}
     return(
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{date}</div>
             <div className='expense-item__description '>
-                <h2>Title</h2>
-                <div className='expense-item__price'>Amount</div>
+                <h2>{title}hello</h2>
+                <div className='expense-item__price'>{amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
     )
 }

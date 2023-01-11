04b75d58f34a090b5d9e21602468f7df1176ce94
Gabriela Papp <gpgabika@gmail.com>
2023-01-11 21:36:21 +0000

    date component created

--- src/App.js
+++ src/App.js
@@ -7,20 +7,21 @@ const initialExpenses = [
     id: 'e1',
     title: 'Toilet Paper',
     amount: 94.12,
+    date:new Date(2020, 7, 14)
     
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, },
+  { id: 'e2', title: 'New TV', amount: 799.49,date:new Date(2020,9,1) },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
-    
+    date:new Date(2020,10,17)
   },
   {
     id: 'e4',
     title: 'New Desk (Wooden)',
     amount: 450,
-    
+    date: new Date(2021,1,31)
   },
 ];
 function App() {
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,21 @@
 import { useState } from 'react'
+import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.style.css'
 
 const ExpenseItem=(props)=>{
-    const [item, setItem]= useState()
-const clickHandler=()=>{
-
-}
-console.log(props.items)
     const {title,date,amount,id}=props.items
+    const [item, setItem]= useState()
     
     return(
                <div>
         {props.items.map(expense=>(
-            
-            <div className='expense-item'>
-               <div>{expense.date}</div>
+             <div className='expense-item' key={expense.id}>
+               <ExpenseDate date={expense.date}/>
                <div className='expense-item__description '>
                    <h2>{expense.title}</h2>
-                   <div className='expense-item__price'>{expense.amount}</div>
+                   <div className='expense-item__price'>£ {expense.amount}</div>
                </div>
-               <button onClick={clickHandler}>Change Title</button>
+               
            </div>
            ))}
             </div>
